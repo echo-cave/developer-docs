05c6599309d098960a3161f73e160b77eb3dc690
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-08-25 07:38:24 +0800

    First!

---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,20 @@
+module.exports = [
+  // Nav 1
+  {
+    text: "首页",
+    link: "/"
+  },
+  {
+    text: "语句接口",
+    link: "/sentence/"
+  },
+  // Nav 2
+  {
+    text: "回声洞相关",
+    items: [
+      { text: "主站", link: "http://cave_v2.rdpstudio.top:31000/" },
+      { text: "统计", link: "http://cave_v2.rdpstudio.top:31000/statics/" },
+    ]
+  },
+  { text: "开源库", link: "https://github.com/echo-cave"}
+];
